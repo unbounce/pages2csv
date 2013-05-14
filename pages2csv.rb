ed3059a6b6873c88063f404661dded3757aad20d
Chris Cummer <chris@unbounce.com>
2013-05-14 11:33:37 -0700

    Now uses OptionParser

--- pages2csv.rb
+++ pages2csv.rb
@@ -2,15 +2,36 @@ require 'awesome_print'
 require 'csv'
 require 'multi_json'
 require 'net/https'
+require 'optparse'
 require 'uri'
 
-class ApiInterface
-  attr_reader :api_key
+class ConfigurationParser
+  attr_accessor :apikey, :subdomain, :offset, :limit
 
-  def initialize(api_key)
-    @api_key    = api_key
+  def initialize(options)
+    @opts = OptionParser.new do |parser|
+      parser.on('-k <apikey>',    '--apikey',     'Your Unbounce API key.')         { |opt| @apikey     = opt }
+      parser.on('-s <subdomain>', '--subdomain',  'Your Subdomain ID.')             { |opt| @subdomain  = opt }
+      parser.on('-o <offset>',    '--offset',     'First record index.')            { |opt| @offset     = opt }
+      parser.on('-l <limit>',     '--limit',      'Number of records to retrieve.') { |opt| @limit      = opt }
+    end
+
+    @opts.parse!(options)
+
+    @offset = 0   if @offset.nil?
+    @limit  = 50  if @limit.nil?
+  end
+end
+
+
+class ApiInterface
+  extend Forwardable
+
+  def initialize(argv)
+    @config = ConfigurationParser.new(argv)
   end
 
+  def_delegators :@config, :apikey, :subdomain, :offset, :limit
 
   def get(uri)
     http = Net::HTTP.new(uri.host, uri.port)
@@ -18,7 +39,7 @@ class ApiInterface
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     request = Net::HTTP::Get.new(uri.request_uri)
-    request.basic_auth(@api_key, '')
+    request.basic_auth(apikey, '')
 
     response = http.request(request)
     response.body
@@ -35,13 +56,11 @@ class UnbouncePage
     @data          = {}
   end
 
-
   def fetch
     json = @api_interface.get(api_uri)
     @data = MultiJson.decode(json)
     self
   end
-
 
   def stats
     return if @data.empty?
@@ -55,7 +74,6 @@ class UnbouncePage
     def api_uri
       URI.parse("https://api.unbounce.com/pages/#{@page_id}")
     end
-
 
     def method_missing(meth, *args, &block)
       data = @data[meth.to_s]
@@ -73,13 +91,9 @@ class UnbouncePages
   attr_reader :api_key, :pages
   attr_accessor :company_id
 
-  def initialize(api_interface, company_id, offset=0, limit=50)
+  def initialize(api_interface)
     @api_interface  = api_interface
-    @company_id     = company_id
-    @offset         = offset
-    @limit          = limit
   end
-
 
   def fetch
     json = @api_interface.get(api_uri)
@@ -88,23 +102,21 @@ class UnbouncePages
     self
   end
 
-
   def as_csv
     CSV.open(file_name, "wb") { |csv| csv << @pages.first.stats.keys }
     CSV.open(file_name, "ab") { |csv| @pages.each { |elem| csv << elem.stats.values.to_a } }
+    file_name
   end
 
-
   def file_name
-    "#{DateTime.now.strftime("%F")}-#{company_id}-page-export.csv"
+    "#{DateTime.now.strftime("%F")}-#{@api_interface.subdomain}-page-export.csv"
   end
 
   private
 
     def api_uri
-      URI.parse("https://api.unbounce.com/sub_accounts/#{@company_id}/pages?offset=#{@offset}&limit=#{@limit}")
+      URI.parse("https://api.unbounce.com/sub_accounts/#{@api_interface.subdomain}/pages?offset=#{@api_interface.offset}&limit=#{@api_interface.limit}")
     end
-
 
     def build_pages(data)
       @pages = data['pages'].map { |page_data| UnbouncePage.new(@api_interface, page_data['id']).fetch }
@@ -117,14 +129,24 @@ end
 # Uses an Unbounce API key and a Subdomain ID to extract data about all the
 # pages in that subdomain.
 #
+# Parameters
+#
+#   Required:
+#     --apikey  Your Unbounce API key.
+#     --subdomain   The numeric ID of the subdomain that has your pages.
+#
+#   Optional:
+#     --offset      The first page to start from. Defaults to 0
+#     --limit       The number of pages to retrieve. Defaults to 50
+#
 # Example:
 #
-#   ruby page2csv.rb 07d92b143345a48b491ee5a6147b848f 47742
+#   ruby pages2csv.rb --apikey 07b491ee5a6147d92b143345a48b848f --subdomain 47742 --offset 50 --limit 100
 #
 if __FILE__ == $0
-  ai = ApiInterface.new(ARGV[0])
-  pe = UnbouncePages.new(ai, ARGV[1], 0, 1000)
-  pe.fetch.as_csv
+  ai = ApiInterface.new(ARGV)
+  up = UnbouncePages.new(ai)
+  up.fetch.as_csv
 
-  puts "Wrote #{pe.file_name}/"
+  puts "Wrote #{up.file_name}/"
 end
